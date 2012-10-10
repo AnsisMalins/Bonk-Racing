ae322717ab1b4bad69b0c88f0d6d7ef8abf22bfb
Ansis Māliņš <ansis.malins@gmail.com>
2012-10-10 15:38:55 +0300

    Polished the Vector class.

--- BonkRacing/Vector.vb
+++ BonkRacing/Vector.vb
@@ -16,14 +16,24 @@ Public Structure Vector
 		pY = CSng(y)
 	End Sub
 
-	Public Sub New(ByVal size As SizeF)
-		pX = size.Width
-		pY = size.Height
+	Public Sub New(ByVal point As Point)
+		pX = point.X
+		pY = point.Y
 	End Sub
 
 	Public Sub New(ByVal point As PointF)
 		pX = point.X
 		pY = point.Y
+	End Sub
+
+	Public Sub New(ByVal size As Size)
+		pX = size.Width
+		pY = size.Height
+	End Sub
+
+	Public Sub New(ByVal size As SizeF)
+		pX = size.Width
+		pY = size.Height
 	End Sub
 
 	Private pX As Single
@@ -40,24 +50,24 @@ Public Structure Vector
 		End Get
 	End Property
 
-	Public Shared Operator +(ByVal a As Vector, ByVal b As Vector) As Vector
-		Return New Vector(a.pX + b.pX, a.pY + b.pY)
+	Public Function GetLength() As Single
+		Return CSng(Math.Sqrt(Me * Me))
+	End Function
+
+	Public Function GetNormal() As Vector
+		Return Me / GetLength()
+	End Function
+
+	Public Overrides Function ToString() As String
+		Return pX.ToString() & ", " & pY.ToString()
+	End Function
+
+	Public Shared Operator +(ByVal a As Vector) As Vector
+		Return a
 	End Operator
 
 	Public Shared Operator -(ByVal a As Vector) As Vector
 		Return New Vector(-a.pX, -a.pY)
-	End Operator
-
-	Public Shared Operator -(ByVal a As Vector, ByVal b As Vector) As Vector
-		Return a + -b
-	End Operator
-
-	Public Shared Widening Operator CType(ByVal a As Vector) As PointF
-		Return New PointF(a.pX, a.pY)
-	End Operator
-
-	Public Shared Widening Operator CType(ByVal a As Vector) As SizeF
-		Return New SizeF(a.pX, a.pY)
 	End Operator
 
 	Public Shared Operator *(ByVal a As Vector, ByVal b As Single) As Vector
@@ -65,50 +75,62 @@ Public Structure Vector
 	End Operator
 
 	Public Shared Operator *(ByVal a As Single, ByVal b As Vector) As Vector
-		Return b * a
+		Return New Vector(a * b.pX, a * b.pY)
+	End Operator
+
+	Public Shared Operator *(ByVal a As Vector, ByVal b As Vector) As Single
+		Return a.pX * b.pX + a.pY * b.pY
 	End Operator
 
 	Public Shared Operator /(ByVal a As Vector, ByVal b As Single) As Vector
 		Return New Vector(a.pX / b, a.pY / b)
 	End Operator
 
-	Public Shared Widening Operator CType(ByVal a As Point) As Vector
-		Return New Vector(a.X, a.Y)
+	Public Shared Operator +(ByVal a As Vector, ByVal b As Vector) As Vector
+		Return New Vector(a.pX + b.pX, a.pY + b.pY)
 	End Operator
 
-	Public Shared Widening Operator CType(ByVal a As SizeF) As Vector
-		Return New Vector(a.Width, a.Height)
+	Public Shared Operator -(ByVal a As Vector, ByVal b As Vector) As Vector
+		Return New Vector(a.pX - b.pX, a.pY - b.pY)
 	End Operator
 
-	Public Shared Widening Operator CType(ByVal a As PointF) As Vector
-		Return New Vector(a.X, a.Y)
+	Public Shared Operator =(ByVal a As Vector, ByVal b As Vector) As Boolean
+		Return a.pX = b.pX AndAlso a.pY = b.pY
 	End Operator
 
-	Public Shared Function Subtract(ByVal a As Point, ByVal b As Point) As Vector
-		Return New Vector(a.X - b.X, a.Y - b.Y)
-	End Function
+	Public Shared Operator <>(ByVal a As Vector, ByVal b As Vector) As Boolean
+		Return a.pX <> b.pX OrElse a.pY <> b.pY
+	End Operator
 
 	Public Shared Widening Operator CType(ByVal a As Vector) As Point
 		Return New Point(CInt(a.pX), CInt(a.pY))
+	End Operator
+
+	Public Shared Widening Operator CType(ByVal a As Vector) As PointF
+		Return New PointF(a.pX, a.pY)
 	End Operator
 
 	Public Shared Widening Operator CType(ByVal a As Vector) As Size
 		Return New Size(CInt(a.pX), CInt(a.pY))
 	End Operator
 
-	Public Shared Widening Operator CType(ByVal a As Size) As Vector
-		Return New Vector(a.Width, a.Height)
+	Public Shared Widening Operator CType(ByVal a As Vector) As SizeF
+		Return New SizeF(a.pX, a.pY)
 	End Operator
 
-	Public Overrides Function ToString() As String
-		Return pX.ToString() & ", " & pY.ToString()
-	End Function
+	Public Shared Widening Operator CType(ByVal a As Point) As Vector
+		Return New Vector(a)
+	End Operator
 
-	Public Function GetLength() As Single
-		Return CSng(Math.Sqrt(Math.Pow(pX, 2) + Math.Pow(pY, 2)))
-	End Function
+	Public Shared Widening Operator CType(ByVal a As PointF) As Vector
+		Return New Vector(a)
+	End Operator
 
-	Public Function GetNormal() As Vector
-		Return Me / GetLength()
-	End Function
+	Public Shared Widening Operator CType(ByVal a As Size) As Vector
+		Return New Vector(a)
+	End Operator
+
+	Public Shared Widening Operator CType(ByVal a As SizeF) As Vector
+		Return New Vector(a)
+	End Operator
 End Structure
